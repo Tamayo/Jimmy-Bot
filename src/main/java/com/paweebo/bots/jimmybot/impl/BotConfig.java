79077ec5cc58089c6bc4f6396b358746a27aff42
zhutt <zachary.m.hutt@gmail.com>
2018-12-27 14:05:22 -0500

    botConfig updates. Changes to audio triggering. Cris is done.

--- src/main/java/com/paweebo/bots/jimmybot/impl/BotConfig.java
+++ src/main/java/com/paweebo/bots/jimmybot/impl/BotConfig.java
@@ -1,16 +1,31 @@
 package com.paweebo.bots.jimmybot.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Mangaloid on 1/26/2018.
  */
 public class BotConfig {
 
+    private List<SoundTrigger> soundTriggers = new ArrayList();
+    private List<SoundTrigger> joinTriggers = new ArrayList();
+
+
     private String voiceChannelName;
     private transient static final XStreamSerializer<BotConfig> xStreamSerializer = new XStreamSerializer<BotConfig>("botConfig.xml",new BotConfig());
 
-    public static BotConfig getBotConfiguration(){
+    static{
         xStreamSerializer.getXStream().alias("configuration",BotConfig.class);
+        xStreamSerializer.getXStream().alias("soundTrigger",SoundTrigger.class);
+        xStreamSerializer.getXStream().ignoreUnknownElements();
+    }
+
+    public static BotConfig getBotConfiguration(){
         return xStreamSerializer.unSerialize();
+    }
+    public static void saveBotConfiguration(BotConfig botConfig){
+        xStreamSerializer.serialize(botConfig);
     }
 
     private BotConfig(){
@@ -24,5 +39,21 @@ public class BotConfig {
 
     public void setVoiceChannelName(String voiceChannelName) {
         this.voiceChannelName = voiceChannelName;
+    }
+
+    public List<SoundTrigger> getSoundTriggers() {
+        return soundTriggers;
+    }
+
+    public void setSoundTriggers(List<SoundTrigger> soundTriggers) {
+        this.soundTriggers = soundTriggers;
+    }
+
+    public List<SoundTrigger> getJoinTriggers() {
+        return joinTriggers;
+    }
+
+    public void setJoinTriggers(List<SoundTrigger> joinTriggers) {
+        this.joinTriggers = joinTriggers;
     }
 }
